54da50e3bf52c8a2437085e20db49a3d605f8f21
jhasmany-fernandez <jhasmany.fernandez.dev@gmail.com>
2025-01-19 09:57:47 -0400

    update

--- 1 - Primero Parcial/1 Programacion Batch/Ejercicios Batch/13 - Modifica-Contenido-Archivo.bat
+++ 1 - Primero Parcial/1 Programacion Batch/Ejercicios Batch/13 - Modifica-Contenido-Archivo.bat
@@ -1,3 +1,7 @@
 :: Función del comando echo: Imprime el texto proporcionado después de la palabra clave echo. En este caso, imprime "Esta es otra linea".
 :: Uso de >>: Redirecciona la salida del comando a un archivo, pero en lugar de sobrescribir el contenido existente (como haría >), agrega el texto al final del archivo. Si el archivo no existe, lo crea.
 echo Esta es otra linea >> texto.txt
+
+:: Respuesta:
+:: Esta es una prueba
+:: Hasta luego
